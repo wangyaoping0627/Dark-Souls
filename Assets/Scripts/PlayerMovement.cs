30ebdba9f978c21396e9db153314ab8615026a45
wangyaoping0627 <wangyaoping0627@gmail.com>
2026-05-27 17:33:33 +0800

    移动

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public NavMeshAgent nav;
+    public GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +15,15 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+{
+    if (Input.GetMouseButtonDown(1)) // 1 = 右键
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            target.transform.position = hit.point;
+            nav.SetDestination(hit.point);
+        }
     }
+}
 }
