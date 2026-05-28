a578ada04d519bde9bfaf641700a98eba76e0e8c
wangyaoping0627 <wangyaoping0627@gmail.com>
2026-05-28 15:24:28 +0800

    PlayerAnim

--- Assets/Scripts/PlayerAnimController.cs
+++ Assets/Scripts/PlayerAnimController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAnimController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator anim;
+    public Rigidbody rb;
+
+    private bool isStanding;
+    private bool isMoving;
     void Start()
     {
-        
+        isStanding = true;
+        isMoving = false;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.magnitude != 0)
+        {
+            isMoving = true;
+            isStanding = false;
+        }
+        else
+        {
+            isMoving = false;
+            isStanding = true; 
+        }
+        ToAnimator();
+    }
+
+    void ToAnimator()
+    {
+        anim.SetBool("isStanding",isStanding);
+        anim.SetBool("isMoving",isMoving);
     }
 }
