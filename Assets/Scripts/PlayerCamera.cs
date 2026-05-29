c39063a090d9c06b1d4bab5500be35643f394eaf
wangyaoping0627 <wangyaoping0627@gmail.com>
2026-05-29 11:12:34 +0800

    Update PlayerCamera.cs

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
@@ -7,29 +5,42 @@ public class PlayerCamera : MonoBehaviour
     public Transform player;
     public Vector3 offset;//摄像机偏移量
     public float rotateSpeed = 3f;
-    public GameObject target;
+    float miny=-95,maxy=0;
+    private float x=0;
+    private float y=0;//x,y记录现在的旋转角度 用来限制镜头旋转
+    float distance;
+    private Vector3 baseDir;   // 记住初始方向
+
     void Start()
     {
-        offset = transform.position - player.position;
+        baseDir = offset.normalized;
+        distance = offset.magnitude;
     }
     void LateUpdate()
     {
-        if(player != null)
+
+        if(player == null)
         {
-            //按鼠标左键旋转相机
-            RotateCamera();
-            //得到相机位置
-            this.transform.position=player.position+offset;
-            transform.LookAt(player);
-        }
+            print("角色为空");
+            return;
+        } 
+        
+        //按鼠标左键旋转相机
+        RotateCamera();
+        //得到相机位置
+        this.transform.position=player.position+offset;
+        transform.LookAt(player);
+
     }
 
     void RotateCamera()
     {
         if (Input.GetMouseButton(0))
         {
-            offset = Quaternion.Euler(0,Input.GetAxis("Mouse X") * rotateSpeed, 0) * offset;
-            offset = Quaternion.Euler(-Input.GetAxis("Mouse Y") * rotateSpeed, 0, 0) * offset;
+            x+=Input.GetAxis("Mouse X")*rotateSpeed;
+            y-=Input.GetAxis("Mouse Y")*rotateSpeed;
+            y=Mathf.Clamp(y,miny,maxy);
+            offset = Quaternion.Euler(y, x, 0) * baseDir * distance;
         }
     }
 }
